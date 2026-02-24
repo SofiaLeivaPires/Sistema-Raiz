8ea7b3a1c68466f1dba7aadfd1d8e894550794c5
Sofia <Sofia.leivapires@gmail.com>
2026-02-24 12:00:27 -0300

    feat(web): Criacao da categoria controller

--- src/Data/ApplicationDbContext.cs
+++ src/Data/ApplicationDbContext.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Raiz.Models;
 
 namespace Raiz.Data;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+public class ApplicationDbContext : IdentityDbContext
 {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<Categoria> Categorias { get; set; }
 }
